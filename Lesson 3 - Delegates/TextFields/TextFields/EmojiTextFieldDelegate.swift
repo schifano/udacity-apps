f61ebcee453e861a3d4540b6db0d4ebaa9dec008
Rachel Schifano <rschifa1@gmail.com>
2015-08-06 15:02:29 -0500

    Implement counter delegate and fix constraints

---
+++ Lesson 3 - Delegates/TextFields/TextFields/EmojiTextFieldDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiTextFieldDelegate.swift
+//  TextFields
+//
+//  Created by Rachel Schifano on 8/6/15.
+//  Copyright (c) 2015 schifano. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class EmojiTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+}
---
+++ Lesson 3 - Delegates/TextFields/TextFields/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  TextFields
+//
+//  Created by Rachel Schifano on 8/6/15.
+//  Copyright (c) 2015 schifano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var emojiTextField: UITextField!
+    @IBOutlet weak var colorTextField: UITextField!
+    @IBOutlet weak var counterTextField: UITextField!
+    @IBOutlet weak var characterCountLabel: UILabel!
+    
+    // Declare delegate objects
+    let emojiTextFieldDelegate = EmojiTextFieldDelegate()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Assign delegate objects to the text fields
+        self.emojiTextField.delegate = emojiTextFieldDelegate
+        self.counterTextField.delegate = self
+    }
+
+    // Delegate method shouldChangeCharactersInRange
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        // Retrieve the current text and update with the new text
+        // Must be of type NSString to use stringByReplacingCharactersInRange
+        var newText: NSString = textField.text
+        newText = newText.stringByReplacingCharactersInRange(range, withString: string)
+        
+        // Hide and show counter label
+        characterCountLabel.hidden = (newText.length == 0)
+        
+        // Write length of new string into label
+        characterCountLabel.text = String(newText.length)
+        
+        // Give textfield permission to update
+        return true
+    }
+}
